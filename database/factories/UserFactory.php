72fd54063a2d58b7346e83e0d94cba3d921eff87
allan muzeya <allan.thecodemaster@gmail.com>
2022-02-11 00:54:04 +0200

    Authentication Scaffolding (Breeze) .........................DONE Initial Database Migration..........................DONE Admin User Factory..................................DONE

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,10 +3,11 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ * @extends Factory
  */
 class UserFactory extends Factory
 {
@@ -15,13 +16,13 @@ class UserFactory extends Factory
      *
      * @return array
      */
-    public function definition()
+    public function definition():array
     {
         return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
+            'name' => env('DEFAULT_APP_ADMIN_USERNAME'),
+            'email' => env('DEFAULT_APP_ADMIN_EMAIL'),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => hash::make(env('DEFAULT_APP_ADMIN_PASSWORD')), // password
             'remember_token' => Str::random(10),
         ];
     }
@@ -29,9 +30,9 @@ class UserFactory extends Factory
     /**
      * Indicate that the model's email address should be unverified.
      *
-     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     * @return Factory
      */
-    public function unverified()
+    public function unverified(): Factory
     {
         return $this->state(function (array $attributes) {
             return [
